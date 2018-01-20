ecf592e01a61a964361e4f0d6c29900cf4b1871b
John Liu <johnliu55tw@gmail.com>
2018-01-21 00:55:17 +0800

    Added basic pomodoro function.

--- app/index.js
+++ app/index.js
@@ -1,42 +1,78 @@
 import clock from 'clock'
-import document from 'document'
 import {vibration} from 'haptics'
-
+import {display} from 'display'
 import * as buttons from 'buttons'
 import * as views from 'views'
+import {PomodoroTimer} from 'pomodoro'
+import {CONFIG} from 'config'
+
+// Creating PomodoroTimer instance
+console.log('Create new PomodoroTimer.')
+let pomo = new PomodoroTimer(CONFIG.pomodoroSettings)
+console.log('Adding notification handler to PomodoroTimer.')
+pomo.onnotify = () => {
+  console.log('NOTIFY!!!')
+  vibration.start('nudge')
+  display.on = true
+}
 
 clock.granularity = 'seconds'
 // Update current time
 clock.addEventListener('tick', (evt) => {
   views.datetime(evt.date)
 })
+// Update Pomodoro view
+clock.addEventListener('tick', (evt) => {
+  pomo.update(evt.date)
+  views.pomodoro(pomo)
+})
 
 buttons.reset().addEventListener('click', (evt) => {
   console.log('reset')
   vibration.start('confirmation')
+  views.hideOptionsMenu()
+
+  console.log('Reset. Create new PomodoroTimer.')
+  pomo.reset() // This is required to clear the notify timer
+  pomo = new PomodoroTimer(CONFIG.pomodoroSettings)
+  console.log('Adding notification handler to PomodoroTimer.')
+  pomo.onnotify = () => {
+    console.log('NOTIFY!!!')
+    vibration.start('nudge')
+    display.on = true
+  }
+  views.pomodoro(pomo)
 })
+
 buttons.skip().addEventListener('click', (evt) => {
   console.log('skip')
   vibration.start('confirmation')
+  views.hideOptionsMenu()
+
+  pomo.skip()
+  pomo.update()
+  views.pomodoro(pomo)
 })
+
 buttons.toggle().addEventListener('click', (evt) => {
   console.log('toggle')
   vibration.start('confirmation')
+  pomo.toggle()
+  pomo.update()
+  views.pomodoro(pomo)
 })
+
 buttons.stat().addEventListener('click', (evt) => {
-  console.log('stat')
+  console.log('stat. Do nothing right now.')
   vibration.start('confirmation')
 })
 
 buttons.flag().addEventListener('click', () => {
-  console.log('Flag clicked.')
+  console.log('flag.')
   vibration.start('confirmation')
-  document.getElementById('options-container').style.visibility = 'visible'
-  document.getElementById('flag-button').style.visibility = 'hidden'
+  views.showOptionsMenu()
   // Close after 3 seconds
   setTimeout(() => {
-    document.getElementById('options-container').style.visibility = 'hidden'
-    document.getElementById('flag-button').style.visibility = 'visible'
-    vibration.start('confirmation')
+    views.hideOptionsMenu()
   }, 3000)
 })
--- app/views.js
+++ app/views.js
@@ -1,25 +1,54 @@
 import document from 'document'
 import * as utils from '../common/utils.js'
+import {PomoIntvlState, PomoTimerState} from 'pomodoro'
 
 export function datetime (date) {
   /* Takes a Date object to update time related views */
   timeText().text = date.getHours().toString() + ':' + date.getMinutes().toString()
 }
 
-export function pomodoro (countdown,
-                          countdownTotal,
-                          finishedIntvls,
-                          totalIntvls,
-                          color = null) {
-  pomoTime().text = utils.secondsToClock(countdown, true)
-  pomoSets().text = finishedIntvls.toString() + '/' + totalIntvls.toString()
+export function pomodoro (pomoTimer) {
+  pomoTime().text = utils.secondsToClock(pomoTimer.countdown / 1000, true)
+  pomoSets().text = pomoTimer.doneIntvls.toString() + '/' +
+                    pomoTimer.totalIntervals.toString()
   // The countdown circle
-  pomoCircle.sweepAngle = (countdown / countdownTotal) * 360
-  if (color) {
+  // XXX Fixed this
+  pomoCircle.sweepAngle = 360
+  if (pomoTimer.timerState === PomoTimerState.running) {
+    playIcon().style.visibility = 'hidden'
+    pauseIcon().style.visibility = 'visible'
+    let color = null
+    switch (pomoTimer.intvlState) {
+      case PomoIntvlState.work:
+        color = 'fb-red'
+        break
+      case PomoIntvlState.rest:
+        color = 'fb-blue'
+        break
+      case PomoIntvlState.longRest:
+        color = 'fb-mint'
+        break
+    }
     pomoTime().style.fill = color
     pomoSets().style.fill = color
     pomoCircle().style.fill = color
+  } else {
+    playIcon().style.visibility = 'visible'
+    pauseIcon().style.visibility = 'hidden'
+    pomoTime().style.fill = 'gray'
+    pomoSets().style.fill = 'gray'
+    pomoCircle().style.fill = 'gray'
   }
+}
+
+export function showOptionsMenu () {
+  optionsContainer().style.visibility = 'visible'
+  flagButton().style.visibility = 'hidden'
+}
+
+export function hideOptionsMenu () {
+  optionsContainer().style.visibility = 'hidden'
+  flagButton().style.visibility = 'visible'
 }
 
 /* Helper functions for getting the elements */
@@ -27,3 +56,7 @@ let timeText = () => document.getElementById('stat-text')
 let pomoTime = () => document.getElementById('countdown-counter')
 let pomoSets = () => document.getElementById('interval-counter')
 let pomoCircle = () => document.getElementById('countdown-arc')
+let playIcon = () => document.getElementById('play-icon')
+let pauseIcon = () => document.getElementById('pause-icon')
+let optionsContainer = () => document.getElementById('options-container')
+let flagButton = () => document.getElementById('flag-button')
