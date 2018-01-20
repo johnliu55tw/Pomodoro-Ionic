e4344e18526c343423b598c8bd28fab00388898f
John Liu <johnliu55tw@gmail.com>
2018-01-20 23:39:18 +0800

    Added config module.

---
+++ app/config.js
@@ -0,0 +1,10 @@
+export const CONFIG = {
+  pomodoroSettings: {
+    work: 2,
+    rest: 1,
+    longRest: 3,
+    longRestAfter: 2,
+    totalIntervals: 4
+  },
+  pomodoroTimerPath: 'pomo_timer'
+}
