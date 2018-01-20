e4c470564de6aca50c23e1aa9abc165cda33e19d
John Liu <johnliu55tw@gmail.com>
2018-01-21 01:03:52 +0800

    Change color rule.

--- app/views.js
+++ app/views.js
@@ -2,6 +2,22 @@ import document from 'document'
 import * as utils from '../common/utils.js'
 import {PomoIntvlState, PomoTimerState} from 'pomodoro'
 
+function getIntvlStateColor (intvlState) {
+  let color = null
+  switch (intvlState) {
+    case PomoIntvlState.work:
+      color = 'fb-red'
+      break
+    case PomoIntvlState.rest:
+      color = 'fb-blue'
+      break
+    case PomoIntvlState.longRest:
+      color = 'fb-mint'
+      break
+  }
+  return color
+}
+
 export function datetime (date) {
   /* Takes a Date object to update time related views */
   timeText().text = date.getHours().toString() + ':' + date.getMinutes().toString()
@@ -14,30 +30,34 @@ export function pomodoro (pomoTimer) {
   // The countdown circle
   // XXX Fixed this
   pomoCircle.sweepAngle = 360
-  if (pomoTimer.timerState === PomoTimerState.running) {
-    playIcon().style.visibility = 'hidden'
-    pauseIcon().style.visibility = 'visible'
-    let color = null
-    switch (pomoTimer.intvlState) {
-      case PomoIntvlState.work:
-        color = 'fb-red'
-        break
-      case PomoIntvlState.rest:
-        color = 'fb-blue'
-        break
-      case PomoIntvlState.longRest:
-        color = 'fb-mint'
-        break
-    }
-    pomoTime().style.fill = color
-    pomoSets().style.fill = color
-    pomoCircle().style.fill = color
-  } else {
-    playIcon().style.visibility = 'visible'
-    pauseIcon().style.visibility = 'hidden'
-    pomoTime().style.fill = 'gray'
-    pomoSets().style.fill = 'gray'
-    pomoCircle().style.fill = 'gray'
+
+  // When paused, only the circle will be gray. The text will be the color indicating
+  // what states it currently on
+  let color = getIntvlStateColor(pomoTimer.intvlState)
+  switch (pomoTimer.timerState) {
+    case PomoTimerState.running:
+      playIcon().style.visibility = 'hidden'
+      pauseIcon().style.visibility = 'visible'
+      pomoTime().style.fill = color
+      pomoSets().style.fill = color
+      pomoCircle().style.fill = color
+      break
+
+    case PomoTimerState.paused:
+      playIcon().style.visibility = 'visible'
+      pauseIcon().style.visibility = 'hidden'
+      pomoTime().style.fill = color
+      pomoSets().style.fill = color
+      pomoCircle().style.fill = 'gray'
+      break
+
+    case PomoTimerState.idle:
+      playIcon().style.visibility = 'visible'
+      pauseIcon().style.visibility = 'hidden'
+      pomoTime().style.fill = 'gray'
+      pomoSets().style.fill = 'gray'
+      pomoCircle().style.fill = 'gray'
+      break
   }
 }
 
